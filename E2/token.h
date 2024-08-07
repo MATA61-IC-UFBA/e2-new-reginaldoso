b176edfdf695b9e23715be84b489c3da7afc405f
reginaldoso <reginaldoso@ufba.br>
2024-08-06 22:18:20 -0300

    reenvio de arquivos da e2

--- E2/token.h
+++ E2/token.h
@@ -1,1 +1,15 @@
 /* token.h */
+
+/*
+Padrão { Ação }
+*/
+
+typedef enum {
+        EOL=0,          // newline (final de linha)   
+        NUMBER,      // 2
+        PLUS,            // 3
+        MINUS,         // 4
+        TIMES,          // 5
+        DIV,               // 6
+        ERROR        // 7
+} token_t; 
